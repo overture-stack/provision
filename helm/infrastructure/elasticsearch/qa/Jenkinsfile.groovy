04684809e49f1b1c9d85be7bf1f44be2ea9e1106
blabadi <bashar.allabadi@oicr.on.ca>
2019-05-23 19:56:03 +0000

    add jenkins for elastic

---
+++ helm/infrastructure/elasticsearch/qa/Jenkinsfile.groovy
@@ -0,0 +1,36 @@
+def commit = "UNKNOWN"
+def version = "UNKNOWN"
+pipeline {
+    agent {
+        kubernetes {
+            label 'maestro-executor'
+            yaml """
+apiVersion: v1
+kind: Pod
+spec:
+  containers:
+  - name: helm
+    image: alpine/helm:2.12.3
+    command:
+    - cat
+    tty: true
+"""
+        }
+    }
+    stages {
+        stage('Deploy') {
+            steps {
+                container('helm') {
+                    withCredentials([file(credentialsId:'4ed1e45c-b552-466b-8f86-729402993e3b', variable: 'KUBECONFIG')]) {
+                        sh 'helm init --client-only'
+                        sh "helm ls --kubeconfig $KUBECONFIG"
+                        sh 'helm repo add elastic https://helm.elastic.co'
+                        sh """
+                            helm upgrade --kubeconfig $KUBECONFIG --install --namespace=overture-qa elasticsearch-qa \\
+                            elastic/elasticsearch -f helm/infrastructure/elasticsearch/qa/values.yaml --version 7.1.0
+                           """
+                    }
+                }
+            }
+        }
+}
